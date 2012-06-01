b999e0ffcaf6832dc19c3b66b6537b8d1dbc8b5a
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-06-01 13:17:42 -0700

    Split communication into namespaces.

--- server.js
+++ server.js
@@ -16,33 +16,39 @@ app.use('/controller/', express.static(__dirname + '/controller/'));
 
 app.listen(8080);
 
-io.sockets.on('connection', function (socket) {
-    // Screens
+var screen_io = io.of('/screens').on('connection', function(socket) {
     socket.on('set screen_id', function(screen_id) {
         if (screens.indexOf(screen_id) == -1) {
             screens.push(screen_id);
         }
         
         socket.set('screen_id', screen_id, function() {
-           io.sockets.emit('updated screen list', screens); 
+           controller_io.emit('updated screen list', screens); 
         });
     });
     
-    // Controllers
+    socket.on('disconnect', function() {
+        socket.get('screen_id', function(err, screen_id) {
+            screens.splice(screens.indexOf(screen_id), 1);
+            controller_io.emit('updated screen list', screens);
+        });
+    });
+});
+
+var controller_io = io.of('/controllers').on('connection', function(socket) {
     socket.on('set controller_id', function (controller_id) {
         if (controllers.indexOf(controller_id) == -1) {
             controllers.push(controller_id);
         }
 
-        socket.set('controller_id', controller_id, function() {
-            io.sockets.emit('updated screen list', screens);
-        });
+        socket.set('controller_id', controller_id);
+        socket.emit('updated screen list', screens);
     });
     
     socket.on('action', function(action) {
         socket.get('controller_id', function(err, controller_id) {
             socket.get('on_screen', function(err, screen_id) {
-                io.sockets.emit('new action ' + screen_id, [controller_id, action]);
+                screen_io.emit('new action ' + screen_id, [controller_id, action]);
             });
         });
     });
@@ -50,26 +56,18 @@ io.sockets.on('connection', function (socket) {
     socket.on('join screen', function(screen_id) {
         socket.set('on_screen', screen_id, function() {
             socket.get('controller_id', function(err, controller_id) {
-                io.sockets.emit('controller joined ' + screen_id, controller_id);
+                screen_io.emit('controller joined ' + screen_id, controller_id);
             });
         });
     });
-
-    // Both
+    
     socket.on('disconnect', function() {
         socket.get('controller_id', function(err, controller_id) {
             controllers.splice(controllers.indexOf(controller_id), 1);
             
             socket.get('on_screen', function(err, screen_id) {
-                io.sockets.emit('controller left ' + screen_id, controller_id);
+                screen_io.emit('controller left ' + screen_id, controller_id);
             });
-        });
-        
-        // this is buggy. screens gets erased when controllers disconnect
-        socket.get('screen_id', function(err, screen_id) {
-            screens.splice(screens.indexOf(screen_id), 1);
-            io.sockets.emit('updated screen list', screens);
         });
     });
 });
-
