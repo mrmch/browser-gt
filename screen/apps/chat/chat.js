e9f2ecd60ff9ee3554e153343ebb4dca8ea4d2c8
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-06-24 01:37:29 -0700

    Created a simple demo chat application! In ~5 minutes.

---
+++ screen/apps/chat/chat.js
@@ -0,0 +1,33 @@
+var APP = (function(app) {
+    app.init = function() {
+        
+    }
+    
+    app.start = function() {
+        $(function() {
+            var base_url = window.location.host;
+            $.get("http://" + base_url + "/screen/apps/" + EMBSCREEN.meta.id + "/screen.html", function(data) {
+                $("body").append(data);
+                $("#qrcode").attr("src", "https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=" + base_url + "/controller/index.html?id=" + EMBSCREEN.screen_id);
+            });
+        });
+    }
+    
+    app.player_joined = function(controller_id) {
+        CUI.joined(controller_id);
+    }
+    
+    app.player_left = function(controller_id) {
+        CUI.left(controller_id);
+    }
+    
+    app.controller_action = function(controller_id, action) {
+        
+    }
+    
+    app.controller_message = function(controller_id, message) {
+        CUI.message(controller_id, message);
+    }
+    
+    return app;
+}(APP || {}));
---
+++ screen/apps/chat/chat.ui.js
@@ -0,0 +1,15 @@
+var CUI = (function(cui) {
+    cui.joined = function(uid) {
+        $("#message_list").append($("<li>User joined: " + uid + "</li>"));
+    };
+    
+    cui.left = function(uid) {
+        $("#message_list").append($("<li>User left: " + uid + "</li>"));
+    };
+    
+    cui.message = function(uid, message) {
+        $("#message_list").append($("<li>" + uid + " said: " + message + "</li>"));
+    };
+    
+    return cui;
+}(CUI || {}));
